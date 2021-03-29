665a4efc51a048ffa16eba5ffd5b4655b8971545
Wesley Paulo <angelportinari17@gmail.com>
2021-03-29 08:28:23 -0300

    First Commit

---
+++ next-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+declare module '*.scss' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.css' {
+  const content: string;
+  export default content;
+}
